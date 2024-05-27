9c1077860617c172e182b5bad3a02586054411b1
VladoLov <vladislav.lovric@gmail.com>
2024-05-27 09:51:09 +0200

    Reinitializing repository and adding files

---
+++ src/features/cart/UpdateItemQuantitiy.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+import { useDispatch } from "react-redux";
+import Button from "../../ui/Button";
+import { decreaseItemQuantity, increaseItemQuantity } from "./cartSlice";
+
+// eslint-disable-next-line react/prop-types
+const UpdateItemQuantitiy = ({ pizzaId, currentQuantity }) => {
+  const dispatch = useDispatch();
+  return (
+    <div className="flex items-center gap-2 md:gap-3">
+      <Button
+        type="round"
+        onClick={() => dispatch(decreaseItemQuantity(pizzaId))}
+      >
+        -
+      </Button>
+      <span className="text-sm font-medium">{currentQuantity}</span>
+      <Button
+        type="round"
+        onClick={() => dispatch(increaseItemQuantity(pizzaId))}
+      >
+        +
+      </Button>
+    </div>
+  );
+};
+
+export default UpdateItemQuantitiy;
